8363a24aca8a514b56ec3905dc088c83dfc16ca8
--globalPeter Springer <peterspringer@seattleacademy.org>
2016-02-04 09:57:16 -0800

    complete looping-through-arrays.js challenge

--- looping-through-arrays.js
+++ looping-through-arrays.js
@@ -22,3 +22,8 @@
      javascripting verify looping-through-arrays.js
 */
 
+var pets = ['cat', 'dog', 'rat'];
+for (var i = pets.length - 1; i >= 0; i--) {
+  pets[i] = pets[i] + 's';
+};
+console.log(pets);
