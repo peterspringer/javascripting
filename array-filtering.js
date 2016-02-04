bb304a0e82b2aaa8837ca10b9b8695474e8b1966
--globalPeter Springer <peterspringer@seattleacademy.org>
2016-02-04 09:48:25 -0800

    complete arrays.js challenge

--- array-filtering.js
+++ array-filtering.js
@@ -23,3 +23,9 @@
 
      javascripting verify array-filtering.js
 */
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var filtered = numbers.filter(function evenNumbers (number) {
+  return number % 2 === 0;
+});
+console.log(filtered);
